9724403f1627d53b771730d9dadf59143126740f
Michael A. Chapman <chapmanm305@gmail.com>
2019-04-30 10:16:22 -0700

    Fix contructor function to work with geo.json data

--- server.js
+++ server.js
@@ -9,19 +9,20 @@ const app = express();
 app.use(cors());
 
 app.get('/location', (request, response) => {
-  let query = request.data.query;
-  let latLong = convertLatLong(query);
+  let locationData = request.query.data;
+  let latLong = convertLatLong(locationData);
   response.send(latLong);
 });
 
 function convertLatLong(query){
   let geoData = require('./data/geo.json');
   let location = {
-    search_query: query, 
-    formatted_query: geoData.results[0].formatted_query,
-    latitudes: geoData.results[0].location.lat,
-    longitude: geoData.results[0].location.lng,
+    search_query: query,
+    formatted_query: geoData.results[0].formatted_address,
+    latitude: geoData.results[0].geometry.location.lat,
+    longitude: geoData.results[0].geometry.location.lng,
   };
   return location;
 }
 
+app.listen(PORT);
